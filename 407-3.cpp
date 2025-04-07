c807e93a591bf9006aed7f677db6cd02ed86d314
jjyeon661 <jeongyeon3361@naver.com>
2025-04-07 14:13:14 +0900

    Add files via upload

---
+++ 407-3.cpp
@@ -0,0 +1,41 @@
+/************************************************************
+*
+*
+* 2개의 2차원 벡터로 외적   을 구하는 프로그램
+*
+* *********************************************************/
+
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+	//0 변수선언
+	double a1, b1 = 0.0;
+	double a2, b2 = 0.0;
+	double cross_product = 0.0;
+
+	//1 x벡터의 x좌표 입력
+	printf("a1의 값을 입력하세요:");
+	scanf_s("%lf", &a1);
+
+	//2 x벡터의 y좌표 입력
+	printf("a2의 값을 입력하세요:");
+	scanf_s("%lf", &a2);
+
+	//3 y벡터의 x좌표 입력
+	printf("b1의 값을 입력하세요:");
+	scanf_s("%lf", &b1);
+
+	//4 y벡터의 y좌표 입력
+	printf("b2의 값을 입력하세요:");
+	scanf_s("%lf", &b2);
+
+	//5 내적 값을 구한다
+	cross_product = a1 * b2 - a2 * b1; 
+
+	//6 결과값을 출력한다
+	printf("내적 계산 결과값은 = %lf \n", cross_product);
+
+	return 0;
+}
